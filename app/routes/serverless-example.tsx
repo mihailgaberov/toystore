8df014a92718d2eba08225685efabdb18281781a
Mihail Gaberov <mihail.gaberov@gmail.com>
2023-09-05 16:31:59 +0200

    :sparkles: add serverless example

---
+++ app/routes/serverless-example.tsx
@@ -0,0 +1,7 @@
+export default function Serverless() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Mickey's Online Toy Exchange</h1>
+    </div>
+  );
+}
